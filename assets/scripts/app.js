f3ce9f574939d222f99efd83da2f5eb821791dab
jesobreira <jefrey@forteras.tech>
2018-01-12 14:10:25 -0200

    temp backend

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,6 @@
 window.VERSION = "1.0.0b1";
-window.BACKEND = "http://104.200.67.85:8880";
-window.CDN = "http://cdn.brecoins.com.br/~bre/";
+window.BACKEND = "https://cdn.brecoins.com.br:8880";
+window.CDN = "https://cdn.brecoins.com.br/~bre/";
 window.EXCHANGE = 1;
 window.common = {
     crypto_currency: {
